ed96533783a17cb0bdf09eaafe88652b57d16f73
nitish516 <ntsh.jain@gmail.com>
2015-05-06 18:05:59 +0530

    Initial commit

--- app/src/main/java/com/pratilipi/pratilipi/SplashScreen.java
+++ app/src/main/java/com/pratilipi/pratilipi/SplashScreen.java
@@ -19,24 +19,9 @@ public class SplashScreen extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
 
-        new Handler().postDelayed(new Runnable() {
-
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
-
-            @Override
-            public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashScreen.this, MainActivity.class);
-                startActivity(i);
-
-                // close this activity
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+        Intent i = new Intent(SplashScreen.this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 
 }
