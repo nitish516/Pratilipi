2ac9930e6c3fc8460ed0a065ca1df6ecdc75632b
Mohit Khaitan <khaitanmkkmohit@gmail.com>
2015-05-31 20:31:31 +0530

    Added More Button Functionality

---
+++ app/src/main/java/com/pratilipi/pratilipi/MoreFeaturedBooks.java
@@ -0,0 +1,17 @@
+package com.pratilipi.pratilipi;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by MOHIT KHAITAN on 31-05-2015.
+ */
+public class MoreFeaturedBooks extends ActionBarActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.more_featured_content);
+    }
+}
