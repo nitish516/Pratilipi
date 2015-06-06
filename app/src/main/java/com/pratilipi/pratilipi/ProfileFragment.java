7f8c585f2eca0f54acfe34e1392ff8beb75d9b5b
bharatwaaj <bharatwaaj.1996@gmail.com>
2015-06-06 17:35:34 +0530

    Added profile Fragment

---
+++ app/src/main/java/com/pratilipi/pratilipi/ProfileFragment.java
@@ -0,0 +1,176 @@
+package com.pratilipi.pratilipi;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ProfileFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ProfileFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProfileFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private ArrayAdapter<String> mForecastAdapter;
+
+    private OnFragmentInteractionListener mListener;
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ProfileFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ProfileFragment newInstance(String param1, String param2) {
+        ProfileFragment fragment = new ProfileFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ProfileFragment()  {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        TextView t1 = (TextView) v.findViewById(R.id.textView_profile);
+        TextView t2 = (TextView) v.findViewById(R.id.textView2_profile);
+        TextView t3 = (TextView) v.findViewById(R.id.textView3_profile);
+        TextView t4 = (TextView) v.findViewById(R.id.textView4_profile);
+        //TextView t5 = (TextView) v.findViewById(R.id.textView5_profile);
+        //TextView t6 = (TextView) v.findViewById(R.id.textView6_profile);
+        //TextView t7 = (TextView) v.findViewById(R.id.textView7_profile);
+        ImageView i = (ImageView) v.findViewById(R.id.imageView_profile);
+        Bitmap bImage = BitmapFactory.decodeResource(this.getResources(), R.drawable.profile_picture);
+        bImage=getRoundedCornerBitmap(bImage,60);
+        i.setImageBitmap(bImage);
+        String[] forecastArray = {
+                "Reset Content Language",
+                "Reset Menu Language",
+                "About Pratilipi",
+        };
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+        mForecastAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.profile_textview,
+                R.id.list_item_profile,
+                weekForecast);
+
+        ListView listView = (ListView) v.findViewById(R.id.listView_profile);
+        listView.setAdapter(mForecastAdapter);
+
+
+        return v;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+
+        }
+    }
+
+    //@Override
+    //public void onAttach(Activity activity) {
+    //   super.onAttach(activity);
+    //  try {
+    //     mListener = (OnFragmentInteractionListener) activity;
+    // } catch (ClassCastException e) {
+    //   throw new ClassCastException(activity.toString()
+    //           + " must implement OnFragmentInteractionListener");
+    //}
+    //}
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap, int pixels) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap
+                .getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        final float roundPx = pixels;
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return output;
+    }
+
+}
